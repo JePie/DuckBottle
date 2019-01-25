6931b984f23ea7352530ea74161c1a0ee75e09f9
Jepi <extrikey@gmail.com>
2019-01-25 12:24:42 -0500

    Memory Check
    The memory check works successfully and outputs everything on console

---
+++ GameEngine_DuckTube/Source/Engine/DuckTrophy/DuckTrophy/Initialise.cpp
@@ -0,0 +1,129 @@
+/*###############################################################
+#																#
+#		Copyright 2019 Duck Trophy All Rights Reserved.			#
+# Developed by Puraj Desai, John Paul Valdez and Minxu Huang    #
+#                                                               #
+################################################################*/
+#include "pch.h"
+#include "Initialise.h"
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <sstream>
+#include<SFML/Graphics.hpp>
+
+using namespace std;
+
+duckTubeEngine::~duckTubeEngine()
+{
+
+}
+
+
+void duckTubeEngine::Run(sf::RenderWindow Window)
+{
+	while (Window.isOpen())
+	{
+		sf::Event event;
+		while (Window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				Window.close();
+			}
+		}
+	}
+}
+
+
+duckTubeEngine::duckTubeEngine()
+{
+
+
+}
+
+
+bool duckTubeEngine::Initialize()
+{
+	if (!SystemRequirements())
+		return false;
+
+	return true;
+}
+
+
+
+bool duckTubeEngine::SystemRequirements()
+{
+	if (!HasFreeDiskSpace())
+		return false;
+
+	return true;
+}
+
+bool  duckTubeEngine::CheckMemory() {
+	bool result = false;
+	DWORDLONG physicalRAMNeeded = 512;
+	DWORDLONG virtualRAMNeeded = 1024;
+	MEMORYSTATUSEX status;
+	status.dwLength = sizeof(status);
+	GlobalMemoryStatusEx(&status);
+	std::string msgTxt;
+
+	cout << "\nPhysical & Virtual Memory Check:\n" << endl;
+	cout << "\tRAM Requested: " << physicalRAMNeeded << "." << endl;
+	cout << "\tVirtual RAM Requested: " << virtualRAMNeeded << "." << endl;
+	cout << "\tThe total physical RAM available: " << status.ullAvailPhys << "." << endl;
+	cout << "\tThe total virtual RAM avaialable: " << status.ullAvailVirtual << ".\n" << endl;
+
+	if (status.ullAvailPhys < physicalRAMNeeded) {
+		cout << "\tMemory Check Failure : Not enough physical memory.\n" << endl;
+
+		result = false;
+	}
+
+	if (status.ullAvailVirtual < virtualRAMNeeded) {
+		cout << "\tMemory Check Failure : Not enough virtual memory.\n" << endl;
+
+		result = false;
+	}
+	result = true;
+	return result;
+}
+bool duckTubeEngine::HasFreeDiskSpace()
+{
+
+	bool result = false;
+	int requiredSpace = 300; //program space spec requirement in MB.
+	ULARGE_INTEGER ulFreeSpace;
+	ULARGE_INTEGER ulTotalSpace;
+	ULARGE_INTEGER ulTotalFreeSpace;
+	std::string msgTxt;
+
+	GetDiskFreeSpaceEx("C:", &ulFreeSpace, &ulTotalSpace, &ulTotalFreeSpace);
+
+	int totalFreeSpaceInMb = (ulTotalFreeSpace.QuadPart / (1024 * 1024));
+
+	if (requiredSpace < totalFreeSpaceInMb)
+	{
+		msgTxt = "Sufficient space";
+		result = true;
+	}
+	else
+	{
+		std::ostringstream buffer;
+		buffer << "Insufficient space. \nDisk space required: " << requiredSpace << "MB" << ".\nDisk space available: " << totalFreeSpaceInMb << "MB";
+
+		msgTxt = buffer.str();
+		result = false;
+	}
+
+	int msgboxID = MessageBox(
+		nullptr,
+		msgTxt.c_str(),
+		"Storage check",
+		MB_ICONINFORMATION
+	);
+
+	return result;
+}
