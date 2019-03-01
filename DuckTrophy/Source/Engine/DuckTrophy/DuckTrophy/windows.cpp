1ec74b45fe5bdcd5a3f83deecc717137b1f0739b
Puraj Desai <purajdesai@yahoo.com>
2019-03-01 14:31:47 -0500

    Conflicts

--- DuckTrophy/Source/Engine/DuckTrophy/DuckTrophy/windows.cpp
+++ DuckTrophy/Source/Engine/DuckTrophy/DuckTrophy/windows.cpp
@@ -70,9 +70,6 @@ void Engine::SlpashScreen() {
 }
 void Engine::mainWindow() 
 {
-	music.openFromFile("ChillingMusic.wav");
-	music.play();
-
 	Window.create(sf::VideoMode(resolution.x, resolution.y),
 		"Main",
 		sf::Style::Default);
@@ -92,7 +89,8 @@ void Engine::mainWindow()
 		//actor.draw();
 		Window.draw(actor.actor);
 
-	
+		music.openFromFile("ChillingMusic.wav");
+		music.play();
 
 		icon.loadFromFile("duck.png");
 		Window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
